6ba5c7cda7ae5c20e03547693fea2c56c8cd3461
Mehdi Abbadi <mehdi.abbadi.pro@gmail.com>
2022-04-15 20:10:28 +0200

    Suppression des duplications en utilisant des layouts

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello city</title>
-    </head>
-    <body>
-        <p>Build with &hearts; By Mehdi</p>
+@section('content')
+    
+    <p>Build with &hearts; By Mehdi</p>
 
-        <p><a href="/">Revenir à la page d'accueil</a></p>
+    <p><a href="/">Revenir à la page d'accueil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello city</title>
-    </head>
-    <body>
-        <h1>Hello from Belgium!</h1>
-        <p>It's currently {{date('H:i')}}.</p>
+@section('content')
+    
+    <h1>Hello from Belgium</h1>
+    <p>Il est actuellement {{date('H:i')}}.</p>
 
-        <footer>
-            <p>&copy; Copyright {{date('Y')}} &middot; <a href="about-us">About us</a></p>
-        </footer>
-    </body>
-</html>
+@endsection
